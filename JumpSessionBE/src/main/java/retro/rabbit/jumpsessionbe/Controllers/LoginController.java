51b9ef4235eabb9a624fa3ecc9371e2d89a95901
CameronPadiachy <cameronpadiachy@icloud.com>
2024-08-17 13:23:28 +0200

    login

--- JumpSessionBE/src/main/java/retro/rabbit/jumpsessionbe/Controllers/LoginController.java
+++ JumpSessionBE/src/main/java/retro/rabbit/jumpsessionbe/Controllers/LoginController.java
@@ -1,5 +1,23 @@
 package retro.rabbit.jumpsessionbe.Controllers;
 
-public class LoginController {
+import org.springframework.web.bind.annotation.*;
+import retro.rabbit.jumpsessionbe.Models.Admin;
+import retro.rabbit.jumpsessionbe.Services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/login")
+@CrossOrigin(origins = "http://localhost:4200")
 
+public class LoginController {
+    @Autowired
+    private LoginService LC = new LoginService();
+    @PostMapping("/post-login_details")
+    public boolean check_login(@RequestBody Admin admin){
+        boolean flag = LC.verify(admin.getUsername(), admin.getPassword());
+        return flag;
+    }
 }
--- JumpSessionBE/src/main/java/retro/rabbit/jumpsessionbe/Services/LoginService.java
+++ JumpSessionBE/src/main/java/retro/rabbit/jumpsessionbe/Services/LoginService.java
@@ -1,5 +1,25 @@
 package retro.rabbit.jumpsessionbe.Services;
 
-public class LoginService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import retro.rabbit.jumpsessionbe.Controllers.AdminControllers;
+import retro.rabbit.jumpsessionbe.Models.Admin;
 
+import java.util.List;
+
+@Service
+public class LoginService {
+    public AdminControllers AC = new AdminControllers();
+    @Autowired
+    private AdminService adminService;
+    public boolean verify (String uname, String p_word){
+        boolean flag=false;
+        List<Admin> list_of_Admins =adminService.getAllAdmins();
+        String expected_u = list_of_Admins.get(0).getUsername();
+        String expected_p = list_of_Admins.get(0).getPassword();
+        if (uname.equals(expected_u) && p_word.equals(expected_p)){
+            flag= true;
+        }
+        return flag;
+    }
 }
